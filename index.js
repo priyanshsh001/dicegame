e5103e292b6cebebf86a148a6b00be70771a195d
Priyanshu <77235055+priyanshsh001@users.noreply.github.com>
2022-12-14 10:22:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var rand=Math.floor(Math.random()*6+1);
+var rand2=Math.floor(Math.random()*6+1);
+console.log(rand);
+console.log(rand2);
+document.querySelector("img.img1 ").setAttribute("src","./images/dice"+rand+".png");
+document.querySelector("img.img2 ").setAttribute("src","./images/dice"+rand2+".png");
+
+if(rand>rand2)
+{
+ document.querySelector(".container h1").innerHTML=" Player1 won";
+}
+else if(rand<rand2)
+{
+    document.querySelector(".container h1").innerHTML="Player2 won";
+}
+else{
+    document.querySelector(".container h1").innerHTML="It's a Draw";
+}
